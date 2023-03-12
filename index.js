31a7b4fd45ce26adc0869a65d022eb7ef0bdf289
Garry <garryisrael02@gmail.com>
2023-03-12 16:39:58 -0400

    tests arrays functions

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+  cats.unshift(name);
+}
+function destructivelyRemoveLastCat( ){
+  cats.pop();
+}
+function destructivelyRemoveFirstCat( ){
+  cats.shift( );
+}
+function appendCat(name){
+   const arr =[...cats, name];
+   return arr;
+}
+function prependCat(name){
+  const arr =[name, ...cats];
+   return arr;
+}
+function removeLastCat( ){
+  const arr = cats.slice(0,2);  
+  return arr; 
+}
+function removeFirstCat( ){
+  const arr = cats.slice(1);
+  return arr;
+}
